553c6b8cdb92cb20bc5934aaf0d93e3fb44dd75c
Amit258012 <amit258012@gmail.com>
2025-12-25 13:23:29 +0530

    Added Reward Entity

--- reward-service/src/main/java/com/swiftpay/reward_service/entity/Reward.java
+++ reward-service/src/main/java/com/swiftpay/reward_service/entity/Reward.java
@@ -1,4 +1,27 @@
 package com.swiftpay.reward_service.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "rewards")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Reward {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Double points;
+    private LocalDateTime sentAt;
+    private Long userId;
+
+    @Column(unique = true)
+    private Long transactionId;
 }
+
+
