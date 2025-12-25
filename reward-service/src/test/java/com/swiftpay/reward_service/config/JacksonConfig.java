cd08b54e6d6b21cad869325223c931b8e8cf4475
Amit258012 <amit258012@gmail.com>
2025-12-25 15:39:24 +0530

    Added Jackson config

--- reward-service/src/test/java/com/swiftpay/reward_service/config/JacksonConfig.java
+++ reward-service/src/test/java/com/swiftpay/reward_service/config/JacksonConfig.java
@@ -1,4 +1,18 @@
 package com.swiftpay.reward_service.config;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
 public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+        return mapper;
+    }
 }
