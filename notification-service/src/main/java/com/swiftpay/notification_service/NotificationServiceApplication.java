c4d68f0167cdbe5923e4eca42cec491f9b6e10e1
Amit258012 <amit258012@gmail.com>
2025-12-25 11:47:38 +0530

    Starting Reward service

--- notification-service/src/main/java/com/swiftpay/notification_service/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/swiftpay/notification_service/NotificationServiceApplication.java
@@ -15,24 +15,24 @@ public class NotificationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
-	@PostConstruct
-	public void testKafkaConnection() {
-		try {
-			Properties props = new Properties();
-			props.put("bootstrap.servers", "localhost:9092");
-			props.put("group.id", "test-connection");
-			props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-			props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-
-			KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-			consumer.listTopics();
-
-			System.out.println("🔥 Notification-service connected to Kafka successfully!");
-			consumer.close();
-		} catch (Exception e) {
-			System.out.println("❌ Notification-service FAILED to connect to Kafka");
-			e.printStackTrace();
-		}
-	}
+//	@PostConstruct
+//	public void testKafkaConnection() {
+//		try {
+//			Properties props = new Properties();
+//			props.put("bootstrap.servers", "localhost:9092");
+//			props.put("group.id", "test-connection");
+//			props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+//			props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+//
+//			KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+//			consumer.listTopics();
+//
+//			System.out.println("🔥 Notification-service connected to Kafka successfully!");
+//			consumer.close();
+//		} catch (Exception e) {
+//			System.out.println("❌ Notification-service FAILED to connect to Kafka");
+//			e.printStackTrace();
+//		}
+//	}
 
 }
