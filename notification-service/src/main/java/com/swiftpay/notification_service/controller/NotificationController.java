6461d63df0782e6a45402749e4f74a59455598cf
Amit258012 <amit258012@gmail.com>
2025-12-27 18:27:25 +0530

    Change the port

--- notification-service/src/main/java/com/swiftpay/notification_service/controller/NotificationController.java
+++ notification-service/src/main/java/com/swiftpay/notification_service/controller/NotificationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/notify")
+@RequestMapping("/api/notifications")
 public class NotificationController {
     @Autowired
     private NotificationService notificationService;
